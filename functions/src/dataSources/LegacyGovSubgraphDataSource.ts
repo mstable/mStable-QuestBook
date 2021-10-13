be8113668e3c5b6de3577415baea0b75ee542754
James Lefrère <james.lefrere@mstable.org>
2021-10-13 12:41:57 +0200

    Migration amount fallback
    - Fallback to the total withdrawn amount to get the previous v1 balance

--- functions/src/dataSources/LegacyGovSubgraphDataSource.ts
+++ functions/src/dataSources/LegacyGovSubgraphDataSource.ts
@@ -1,4 +1,5 @@
 import { SubgraphDataSource } from './SubgraphDataSource'
+import { BigNumber, utils } from 'ethers'
 
 export class LegacyGovSubgraphDataSource extends SubgraphDataSource {
   static create() {
@@ -18,19 +19,31 @@ export class LegacyGovSubgraphDataSource extends SubgraphDataSource {
 
   async mostRecentStakeAmount(account: string): Promise<number> {
     const blockAtExpiry = 13261953
-    const result = await this.query<{ userLockups: { id: string; value: string }[] }>(
+    const result = await this.query<{
+      userLockups: { id: string; value: string }[]
+      withdrawTransactions: { id: string; value: string }[]
+    }>(
       `query {
           userLockups(where: { account: "${account}", value_gt: 0 }, block: { number: ${blockAtExpiry} }) {
               id
               value
           }
+          withdrawTransactions(where: { sender: "${account}" }) { id value }
       }`,
     )
 
-    const { userLockups } = result.body.data
+    const { userLockups, withdrawTransactions } = result.body.data
 
     if (!userLockups[0]?.id) {
-      return 0
+      if (!withdrawTransactions[0]?.id) {
+        return 0
+      }
+
+      // If they don't have balance at this block, they may have withdrawn;
+      // take the total withdrawn value.
+      // Unlikely they staked all/withdrew all/staked all, so should be appropriate
+      const totalWithdrawn = withdrawTransactions.reduce((prev, { value }) => prev.add(BigNumber.from(value)), BigNumber.from(0))
+      return parseFloat(utils.formatUnits(totalWithdrawn))
     }
 
     return parseInt(userLockups[0]?.value ?? '0') / 1e18
