db2f38836360fb28b68a9be4b15940f9e81446d8
James Lefrère <james.lefrere@mstable.org>
2021-10-13 12:53:49 +0200

    Use locked amount total

--- functions/src/dataSources/LegacyGovSubgraphDataSource.ts
+++ functions/src/dataSources/LegacyGovSubgraphDataSource.ts
@@ -21,29 +21,32 @@ export class LegacyGovSubgraphDataSource extends SubgraphDataSource {
     const blockAtExpiry = 13261953
     const result = await this.query<{
       userLockups: { id: string; value: string }[]
-      withdrawTransactions: { id: string; value: string }[]
+      increaseLockAmountTransactions: { id: string; value: string }[]
+      createLockTransactions: { id: string; value: string }[]
     }>(
       `query {
           userLockups(where: { account: "${account}", value_gt: 0 }, block: { number: ${blockAtExpiry} }) {
               id
               value
           }
-          withdrawTransactions(where: { sender: "${account}" }) { id value }
+          createLockTransactions(where: { sender: "${account}" }) { id value }
+          increaseLockAmountTransactions(where: { sender: "${account}" }) { id value }
       }`,
     )
 
-    const { userLockups, withdrawTransactions } = result.body.data
+    const { userLockups, createLockTransactions = [], increaseLockAmountTransactions = [] } = result.body.data
 
     if (!userLockups[0]?.id) {
-      if (!withdrawTransactions[0]?.id) {
+      if (!createLockTransactions[0]?.id && !increaseLockAmountTransactions[0]?.id) {
         return 0
       }
 
-      // If they don't have balance at this block, they may have withdrawn;
-      // take the total withdrawn value.
-      // Unlikely they staked all/withdrew all/staked all, so should be appropriate
-      const totalWithdrawn = withdrawTransactions.reduce((prev, { value }) => prev.add(BigNumber.from(value)), BigNumber.from(0))
-      return parseFloat(utils.formatUnits(totalWithdrawn))
+      // If they don't have balance at this block, use lock transactions
+      // to approximate a staked amount
+      const lockedTotal = createLockTransactions.reduce((prev, { value }) => prev.add(BigNumber.from(value)), BigNumber.from(0))
+      const increasedTotal = increaseLockAmountTransactions.reduce((prev, { value }) => prev.add(BigNumber.from(value)), BigNumber.from(0))
+      const total = lockedTotal.add(increasedTotal)
+      return parseFloat(utils.formatUnits(total))
     }
 
     return parseInt(userLockups[0]?.value ?? '0') / 1e18
