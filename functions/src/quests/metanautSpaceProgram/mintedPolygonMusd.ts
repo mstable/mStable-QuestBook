3d51e78459df3c0237c4eeb6322f72de0ff3524d
James Lefrère <james.lefrere@mstable.org>
2022-01-12 14:11:27 +0100

    Complete Polygon mUSD mint when polygon saver

--- functions/src/quests/metanautSpaceProgram/mintedPolygonMusd.ts
+++ functions/src/quests/metanautSpaceProgram/mintedPolygonMusd.ts
@@ -1,4 +1,5 @@
 import { QuestObjective } from '../types'
+import { saverPolygon } from './saverPolygon'
 
 export const mintedPolygonMusd: QuestObjective = {
   id: 'mintedPolygonMusd',
@@ -6,6 +7,17 @@ export const mintedPolygonMusd: QuestObjective = {
   description: 'We like the stablecoins (on Polygon).',
   points: 10,
   async checker(account, delegates, dataSources) {
+    const saverObjective = await saverPolygon.checker(account, delegates, dataSources)
+
+    // If the user made any progress on the saver objective, it means they minted mUSD.
+    // This is useful because wrapper contracts won't link a mint to the user's account.
+    if (saverObjective.progress && saverObjective.progress > 0) {
+      return {
+        complete: true,
+        progress: 1,
+      }
+    }
+
     const complete = await dataSources.polygonProtocolSubgraph.didMintMusd(account)
     return {
       complete,
